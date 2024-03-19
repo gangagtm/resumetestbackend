0ad25846067ec9daecf14a5e63bde64ecd016e2d
Ganga Gautam <gangagautam59@gmail.com>
2024-03-18 22:55:38 -0500

    remove comments

--- resumetest/tests/test_app.py
+++ resumetest/tests/test_app.py
@@ -3,13 +3,6 @@ import unittest
 from resumetest.viewcount.app import lambda_handler
 import json
       
-# class TestVisitor(unittest.TestCase):
-#     def test_lambda_handler(self):
-#         res_api = lambda_handler('a', 'b')
-#         self.assertEqual(res_api['statusCode'],200,msg=None)
-        
-    #need add to pipeline python3 -m unittest test.py
-
 
 def test_lambda_handler():
     res_api = lambda_handler('a', 'b')
